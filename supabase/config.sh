a6dc8e8dae0c21305ea5c85a877bd67f309e9254
Markus Jylhänkangas <markus@jylhis.com>
2026-03-27 20:58:34 +0100

    Migrate telemetry to new Supabase project

--- supabase/config.sh
+++ supabase/config.sh
@@ -4,5 +4,5 @@
 # RLS denies all access to the anon key. All reads and writes go through
 # edge functions (which use SUPABASE_SERVICE_ROLE_KEY server-side).
 
-JSTACK_SUPABASE_URL="https://frugpmstpnojnhfyimgv.supabase.co"
-JSTACK_SUPABASE_ANON_KEY="sb_publishable_tR4i6cyMIrYTE3s6OyHGHw_ppx2p6WK"
+JSTACK_SUPABASE_URL="https://dgoyvrnzxpiailzearqg.supabase.co"
+JSTACK_SUPABASE_ANON_KEY="sb_publishable_e-C-_r1YjjtL7l71v-FNDw_1xWIYzrQ"
